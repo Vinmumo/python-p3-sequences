cc1fce048d7f35bf17aeebe0eceaca7238bf92ca
vincent <vinnymummo@gmail.com>
2024-05-25 17:25:26 +0300

    commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    i = 0
+    fib = []
+    while i < length:
+        if i == 0:
+            fib.append(0)
+        elif i == 1:
+            fib.append(1)
+        else:
+            fib.append(fib[i-1] + fib[i-2])
+        i += 1
+    print(fib)
